30f84c0e5a246a7392792399b5d2d138eadf8475
崔云舟 <cuiyunzhou@cuiyunzhoudemacbook-air.local>
2016-06-17 12:28:37 +0800

    create a dog type

---
+++ hehe/hehe/dog.h
@@ -0,0 +1,13 @@
+//
+//  dog.h
+//  hehe
+//
+//  Created by 崔云舟 on 16/6/17.
+//  Copyright © 2016年 崔云舟. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface dog : NSObject
+
+@end
